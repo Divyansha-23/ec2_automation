546c83eab5e92a848348ac73e4597f1c964f971e
Divyansha <divyanshabikkumalla23@gmail.com>
2026-03-16 11:48:55 +0000

    added new changes in html

--- windows/windows2.sh
+++ windows/windows2.sh
@@ -1,22 +1,68 @@
 <powershell>
  
+$hostname = $env:COMPUTERNAME
+$time = Get-Date
+$message = "HTML page created successfully"
+ 
 $html = @"
+ 
 <!DOCTYPE html>
 <html>
+ 
 <head>
-<title>Welcome page</title>
+<title>Windows Instance Dashboard</title>
+ 
+<style>
+body{
+    font-family: Arial;
+    background-color:#f2f2f2;
+    text-align:center;
+}
+ 
+.container{
+    margin-top:120px;
+    background:white;
+    padding:30px;
+    width:450px;
+    margin:auto;
+    border-radius:10px;
+    box-shadow:0 0 10px gray;
+}
+ 
+h1{
+    color:#2c3e50;
+}
+ 
+p{
+    font-size:18px;
+}
+</style>
+ 
 </head>
+ 
 <body>
-<p> Hello !!</p>
+ 
+<div class="container">
+ 
+<h1>Hello from Windows Instance</h1>
+ 
+<p><b>Instance Name:</b> $hostname</p>
+ 
+<p><b>Page Generated At:</b> $time</p>
+ 
+<p><b>Status:</b> $message</p>
+ 
+</div>
+ 
 </body>
+ 
 </html>
+ 
 "@
  
 Set-Content -Path "C:\index.html" -Value $html
  
-"Extended HTML page created successfully" | Out-File C:\windows2.txt
+$message | Out-File C:\windows2.txt
  
 </powershell>
-
-
-
+ 
